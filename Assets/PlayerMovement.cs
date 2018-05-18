7af600703c4cc7f64d61d42f4581c75ddf01cfaa
Honayme <honayme@gmail.com>
2018-05-18 15:50:08 +0200

    Change script by the given one within the drive and then don't have to click on the shapes to make them disappear

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+
+    public Transform transform;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void moveX(float x)
+    {
+        transform.position = new Vector3(x, 0, 0);
+    }
+
+    public void moveY(float z)
+    {
+        transform.position = new Vector3(0, 0, z);
+    }
+}
