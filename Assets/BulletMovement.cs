6430f496a0c57c41ac9f1bc563acdfaee86519e0
Honayme <honayme@gmail.com>
2018-05-29 14:52:38 +0200

    [scene] bullet is proper and well create

--- Assets/BulletMovement.cs
+++ Assets/BulletMovement.cs
@@ -1,16 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
-public class BulletMovement : MonoBehaviour {
+public class BulletMovement : MonoBehaviour
+{
 
+	public float speed = 20.0f; 
+	
 	// Use this for initialization
 	void Start () {
 		
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		
+	void FixedUpdate ()
+	{
+		transform.position += transform.forward * speed * Time.fixedDeltaTime; 
 	}
 }
